80a320fe70e1da073fb258943e82803b480cfc9b
yashagraawal <yash.agraawal@gmail.com>
2024-02-29 17:47:19 +0530

    changess

--- Project/Emp-Management/src/main/java/com/miniproj/repository/EmpRepo.java
+++
@@ -1,7 +0,0 @@
-package com.miniproj.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.miniproj.entity.Employee;
-
-public interface EmpRepo extends JpaRepository<Employee, Long> {
-}
